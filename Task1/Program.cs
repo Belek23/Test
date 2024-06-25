1ba3c7897f00d85cadb614837400aaecfb0c6d64
Belek Abdy <beshik2013@yandex.com>
2024-06-25 13:53:57 +0300

    add lesson:task1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выводим значения разных переменных
+
+
+int value = 100;
+Console.WriteLine(value); 
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
+
