b8deebcb49c448793989dc3ba2f56d5c7b463d9f
Belek Abdy <beshik2013@yandex.com>
2024-06-25 16:00:27 +0300

    add gitignore fail

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выводим макс число из 2-х переменных
+
+int a = 345;
+int b = 354;
+
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else{
+    Console.WriteLine(b);
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Выводим макс значение из 5-и переменных
+
+int a = 61;
+int b = 72;
+int c = 95;
+int d = 44;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+ }
+ if (c > max)
+{
+    max = c;
+ }
+ if (d > max)
+{
+    max = d;
+ }
+ if (e > max)
+{
+    max = e;
+ }
+ Console.WriteLine(max);
+
+ 
+ 
