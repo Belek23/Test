9a877e19cb09a05f02d4859177e5dbbc08ca4e36
Belek Abdy <beshik2013@yandex.com>
2024-06-25 16:23:16 +0300

    add fails

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,8 +1,10 @@
 ﻿// Выводим значения разных переменных
 
 
-int value = 100;
+int value = 100; // -2 10*31 ...2 10*31
+long valuea = 9999; // -2 10*63 ... 2 10*63 целочисленные типы данных 
 Console.WriteLine(value); 
+System.Console.WriteLine(valuea);
 
 double d = 5.55;
 char c = '!';
@@ -10,4 +12,5 @@ bool b = true;
 Console.WriteLine(d);
 Console.WriteLine(c);
 Console.WriteLine(b);
+
 
